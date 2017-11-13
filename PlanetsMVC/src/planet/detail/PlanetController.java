db8d3dabc41c280a86203e19521c36f8f81e10d0
LungJoe <lungjoe107@gmail.com>
2017-11-12 20:19:36 -0600

    formats the mi field

--- PlanetsMVC/src/planet/detail/PlanetController.java
+++ PlanetsMVC/src/planet/detail/PlanetController.java
@@ -7,6 +7,7 @@ import java.util.ResourceBundle;
 import javax.imageio.ImageIO;
 import Exceptions.ExceptionHandler;
 import Models.Planet;
+import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
@@ -26,33 +27,25 @@ import javafx.stage.Stage;
 public class PlanetController implements Initializable {
 
 	@FXML private ImageView planetImage;
-
 	@FXML private Button selectImageButton;
-
 	@FXML private TextField planetName;
-
 	@FXML private TextField planetDiameterKM;
-
 	@FXML private TextField planetDiameterM;
-
 	@FXML private TextField planetMeanSurfaceTempC;
-
 	@FXML private TextField planetMeanSurfaceTempF;
-
 	@FXML private TextField planetNumberOfMoons;
-
 	@FXML private Label fancyPlanetName;
-	
+
 	private Stage stage;
 	private Planet planet = new Planet();
 	private ExceptionHandler handler;
-	
+
 	public PlanetController(Stage stage, Planet planet) {
 		this.stage = stage;
 		this.planet = planet;
 		handler = new ExceptionHandler();
 	}
-	
+
 	@FXML
 	void selectImage(ActionEvent event) {
 		try {
@@ -60,121 +53,119 @@ public class PlanetController implements Initializable {
 			fileChooser.setTitle("Open Resource File");
 			File file = fileChooser.showOpenDialog(stage);
 
-            BufferedImage bufferedImage = ImageIO.read(file);
-            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-            planetImage.setImage(image);
-        } catch (Exception ex) {}
+			BufferedImage bufferedImage = ImageIO.read(file);
+			Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+			planetImage.setImage(image);
+		} catch (Exception ex) {
+		}
 	}
 
 	@FXML
 	void loadPlanet(ActionEvent event) {
 		Alert alert = new Alert(AlertType.INFORMATION);
-    	alert.setTitle("CAUTION");
-    	alert.setHeaderText("Loading a planet file will erase current data.");
-    	alert.setContentText("Contunue?");
-    	alert.showAndWait();
+		alert.setTitle("CAUTION");
+		alert.setHeaderText("Loading a planet file will erase current data.");
+		alert.setContentText("Contunue?");
+		alert.showAndWait();
 
-    	if(alert.getResult() != null){
-    		planet.loadPlanet();
-    		updateTextBoxes();
-    		updateImage();
-    	}
+		if (alert.getResult() != null) {
+			planet.loadPlanet();
+			updateTextBoxes();
+			updateImage();
+		}
 	}
 
 	@FXML
 	void savePlanet(ActionEvent event) {
-		try{
+		try {
 			planet.setPlanetDiameterKm(planetDiameterKM.getText());
 			planet.setPlanetSurfaceTempC(planetMeanSurfaceTempC.getText());
 			planet.setNumMoons(planetNumberOfMoons.getText());
-		}catch(Exception e){
+		} catch (Exception e) {
 			handler.handlePlanetException(e);
 		}
 		planet.savePlanet();
 	}
-	private void updateTextBoxes(){
+
+	private void updateTextBoxes() {
 		planetName.setText(planet.getPlanetName());
 		planetDiameterKM.setText(Double.toString(planet.getPlanetDiameterKm()));
 		planetMeanSurfaceTempC.setText(Double.toString(planet.getPlanetSurfaceTempC()));
 		planetNumberOfMoons.setText(Integer.toString(planet.getNumMoons()));
 	}
-	
-	private void updateImage(){
+
+	private void updateImage() {
 		try {
 			File file;
-			if(planet.getImageFilePath() == null){
+			if (planet.getImageFilePath() == null) {
 				file = new File("images\\no_image.png");
-			}
-			else if(planet.getImageFilePath().equals("default")){
+			} else if (planet.getImageFilePath().equals("default")) {
 				file = new File("images\\neptune.png");
-			}
-			else{
+			} else {
 				file = new File(planet.getImageFilePath());
 			}
-            BufferedImage bufferedImage = ImageIO.read(file);
-            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-            planetImage.setImage(image);
-            planet.setImageFilePath(file.getAbsolutePath());
-        } catch (Exception ex) {}
+			BufferedImage bufferedImage = ImageIO.read(file);
+			Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+			planetImage.setImage(image);
+			planet.setImageFilePath(file.getAbsolutePath());
+		} catch (Exception ex) {
+		}
 	}
 
 	public void initialize(URL location, ResourceBundle resources) {
 		updateImage();
 		updateTextBoxes();
-		
-    	fancyPlanetName.textProperty().bind(planet.firstNameProperty());
-    	planetDiameterM.textProperty().bind(planet.diameterMProperty().asString());
-    	planetMeanSurfaceTempF.textProperty().bind(planet.tempFProperty().asString());	
-    	
-    	
- 
-    	planetName.textProperty().addListener(new PlanetNameChangeListener());
-    	planetDiameterKM.textProperty().addListener(new PlanetDiameterChangeListener());
-    	planetMeanSurfaceTempC.textProperty().addListener(new PlanetSurfaceTempListener());
-    	planetNumberOfMoons.textProperty().addListener(new PlanetMoonCountListener());
+
+		fancyPlanetName.textProperty().bind(planet.firstNameProperty());
+		planetDiameterM.textProperty().bind(planet.diameterMProperty().asString("%,.2f"));
+		planetMeanSurfaceTempF.textProperty().bind(planet.tempFProperty().asString());
+		planetName.textProperty().addListener(new PlanetNameChangeListener());
+		planetDiameterKM.textProperty().addListener(new PlanetDiameterChangeListener());
+		planetMeanSurfaceTempC.textProperty().addListener(new PlanetSurfaceTempListener());
+		planetNumberOfMoons.textProperty().addListener(new PlanetMoonCountListener());
 	}
-	
-    private class PlanetNameChangeListener implements ChangeListener<String> {
+
+	private class PlanetNameChangeListener implements ChangeListener<String> {
 		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-			
 			try {
 				planet.setPlanetName(newValue);
-			} catch(Exception e) {
-			    handler.handlePlanetException(e);
+			} catch (Exception e) {
+				handler.handlePlanetException(e);
 				planetName.setText(oldValue);
 			}
 		}
-    }
-    private class PlanetDiameterChangeListener implements ChangeListener<String> {
+	}
+
+	private class PlanetDiameterChangeListener implements ChangeListener<String> {
 		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 			try {
-				planet.setPlanetDiameterKm(newValue);
-			} catch(Exception e) {
-			    	handler.handlePlanetException(e);
-			    	planetDiameterKM.setText(oldValue);
+				planet.setPlanetDiameterKm(newValue.replaceAll(",", ""));
+			} catch (Exception e) {
+				handler.handlePlanetException(e);
+				planetDiameterKM.setText(oldValue);
 			}
 		}
-    }
-    
-    private class PlanetSurfaceTempListener implements ChangeListener<String> {
+	}
+
+	private class PlanetSurfaceTempListener implements ChangeListener<String> {
 		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 			try {
 				planet.setPlanetSurfaceTempC(newValue);
-			} catch(Exception e) {
-			    	handler.handlePlanetException(e);
-			    	planetMeanSurfaceTempC.setText(oldValue);
+			} catch (Exception e) {
+				handler.handlePlanetException(e);
+				planetMeanSurfaceTempC.setText(oldValue);
 			}
 		}
-    }
-    
-    private class PlanetMoonCountListener implements ChangeListener<String> {
+	}
+
+	private class PlanetMoonCountListener implements ChangeListener<String> {
 		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 			try {
-				planet.setNumMoons(newValue);
-			} catch(Exception e) {
-			    	handler.handlePlanetException(e);
-			    	planetNumberOfMoons.setText(oldValue);
+				planet.setNumMoons(newValue.replaceAll(",", ""));
+			} catch (Exception e) {
+				handler.handlePlanetException(e);
+				planetNumberOfMoons.setText(oldValue);
 			}
 		}
-    }
+	}
 }
